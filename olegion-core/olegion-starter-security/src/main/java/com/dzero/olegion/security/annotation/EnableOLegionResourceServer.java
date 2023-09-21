52b008c27bf95d3abcf7b622b7975e0cf4a3b7dc
DZero <dzd624175217@gmail.com>
2023-09-21 09:46:26 +0800

    update pom.xml

--- olegion-core/olegion-starter-security/src/main/java/com/dzero/olegion/security/annotation/EnableOLegionResourceServer.java
+++ olegion-core/olegion-starter-security/src/main/java/com/dzero/olegion/security/annotation/EnableOLegionResourceServer.java
@@ -1,8 +1,15 @@
 package com.dzero.olegion.security.annotation;
 
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import java.lang.annotation.*;
+
+/**
+ * @author DZero
+ */
 @Documented
 @Inherited
-@Target({ ElementType.TYPE })
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public @interface EnableOLegionResourceServer {
